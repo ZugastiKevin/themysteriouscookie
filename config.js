4498363fe6775b1ccdf2416cad2224e5ad051d38
ZugastiKevin <40165958+ZugastiKevin@users.noreply.github.com>
2023-11-16 20:17:42 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL:
+    "https://themysteriouscookie.bearded-developer.workers.dev",
+  VITE_TMDB_READ_API_KEY:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDEzYjJiNGYzYjg2NTkxNzM4MDA3NGI5Mzc5YjVjYiIsInN1YiI6IjY1NTY2ODdmZWE4NGM3MTA5MTBjNTg3NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WB4KK9c-Ot_Y_j5lxVosVeNK6JSkIEXw0kyNq4OYNLw",
+};
